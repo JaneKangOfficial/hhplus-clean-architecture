0443c03b212f927eab2dd9e373516ab65486544f
tuture <tuture@tuture-MacBook-Air.local>
2024-06-23 13:04:07 +0900

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,95 @@
+plugins {
+    java
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.spring.dependency.management)
+    id("jacoco")
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+allprojects {
+    group = property("app.group").toString()
+}
+
+dependencyManagement {
+    imports {
+        mavenBom(libs.spring.cloud.dependencies.get().toString())
+    }
+}
+
+dependencies {
+    implementation(libs.spring.boot.starter.web)
+    compileOnly(libs.lombok)
+    annotationProcessor(libs.lombok)
+    annotationProcessor(libs.spring.boot.configuration.processor)
+    testImplementation(libs.spring.boot.starter.test)
+}
+
+// about source and compilation
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+with(extensions.getByType(JacocoPluginExtension::class.java)) {
+    toolVersion = "0.8.7"
+}
+
+// bundling tasks
+tasks.getByName("bootJar") {
+    enabled = true
+}
+tasks.getByName("jar") {
+    enabled = false
+}
+// test tasks
+tasks.test {
+    ignoreFailures = true
+    useJUnitPlatform()
+}
+
+
+
+/*
+plugins {
+    java
+    id("org.springframework.boot") version "3.3.1"
+    id("io.spring.dependency-management") version "1.1.5"
+}
+
+group = "io.hhplus.clean"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-web-services")
+    compileOnly("org.projectlombok:lombok")
+    runtimeOnly("com.h2database:h2")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+*/
---
+++ src/test/java/io/hhplus/clean/hhpluscleanarchitecture/HhplusCleanArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hhplus.clean.hhpluscleanarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhplusCleanArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
